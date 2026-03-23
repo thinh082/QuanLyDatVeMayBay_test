e6bc6fdf381f97bdc9c6777a5b9fb278e95bc75d
thinh082 <oko93780@gmail.com>
2026-03-23 19:09:02 +0700

    .

--- QuanLyDatVeMayBay/Services/RedisService.cs
+++ QuanLyDatVeMayBay/Services/RedisService.cs
@@ -8,7 +8,16 @@ namespace QuanLyDatVeMayBay.Services
 
         public RedisService()
         {
-            var redis = ConnectionMultiplexer.Connect("localhost:6379");
+            // Cấu hình chi tiết cho Upstash (hỗ trợ TLS và Password)
+            var options = new ConfigurationOptions
+            {
+                EndPoints = { "eternal-rattler-78535.upstash.io:6379" },
+                Password = "gQAAAAAAATLHAAIncDE5YTc5Mjg2Nzk4NTE0OWM4YmEwZTQwNmFiZjZjOTZhN3AxNzg1MzU",
+                Ssl = true, // Upstash yêu cầu TLS nên phải bật Ssl = true
+                AbortOnConnectFail = false,
+            };
+
+            var redis = ConnectionMultiplexer.Connect(options);
             _db = redis.GetDatabase();
         }
 
